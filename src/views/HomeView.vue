653d7c45fea6c3dbcd9921fa425fc02f23b1f2e1
VivienLN <vivienleneez@gmail.com>
2023-02-07 21:54:52 +0100

    mousewheelevent

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,7 @@
 <script setup>
   import Earth3d from '@/components/Earth3d.vue'
   import destinations from '@/data/destinations.json'
-  import { reactive, ref } from 'vue'
+  import { onMounted, onUnmounted, reactive, ref } from 'vue'
 
   const state = reactive({
     current: {},
@@ -10,8 +10,37 @@
   })
   const index = ref(0)
 
+  // First update
   updateCurrent()
 
+  // Mouse wheel event with throttle
+  const WHEEL_THROTTLE = 800
+  var wheelThrottleTimeout = null
+  var wheelThrottle = false
+
+  function onMousewheel(event) {
+    if (wheelThrottle) {
+      return
+    }
+
+    wheelThrottle = true
+
+    event.deltaY > 0 ? next() : previous()
+
+    wheelThrottleTimeout = setTimeout(() => {
+      wheelThrottle = false
+    }, WHEEL_THROTTLE)
+  }
+
+  // Mounted / unmounted events
+  onMounted(() => {
+    // Nothing
+  })
+  onUnmounted(() => {
+    clearTimeout(wheelThrottleTimeout)
+  })
+
+  // Navigation functions
   function previous() {
     index.value = getPreviousIndex()
     updateCurrent()
@@ -46,6 +75,7 @@
       :previous="state.previous"
       :next="state.next"
       v-bind="state.current"
+      @wheel.passive="onMousewheel"
       @clickPrevious="previous"
       @clickNext="next"
     />
